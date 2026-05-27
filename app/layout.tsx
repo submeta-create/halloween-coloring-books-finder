359ca81aa8d0df88f6171d5b3c320b958092aca9
IBT <ibt@Mac.fritz.box>
2026-05-27 18:30:15 +0200

    add social preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,46 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
-  title: "Halloween Coloring Books",
+  title: "Cozy Halloween Coloring Books",
   description:
-    "Discover cute, cozy, spooky, and relaxing Halloween coloring books for kids, teens, and adults.",
+    "Cute, spooky and cozy coloring books for kids, teens and adults.",
+
+  openGraph: {
+    title: "Cozy Halloween Coloring Books",
+    description:
+      "Cute, spooky and cozy coloring books for kids, teens and adults.",
+    url: "https://halloween-coloring-books-finder.vercel.app",
+    siteName: "Ella Tarling",
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Cozy Halloween Coloring Books",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Cozy Halloween Coloring Books",
+    description:
+      "Cute, spooky and cozy coloring books for kids, teens and adults.",
+    images: ["/og-image.jpg"],
+  },
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-        <Analytics />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
